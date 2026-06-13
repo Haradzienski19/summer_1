4dd06b1f7da9540d7a5447bd43e6c7df25c37b65
Apolajko Alyaksandr <apolajko_ai_23@mf.grsu.by>
2026-06-13 03:06:41 +0300

    add User DTOs with input data validation

---
+++ src/main/java/com/example/demo/dto/UserRequestDTO.java
@@ -0,0 +1,37 @@
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UserRequestDTO {
+
+    @NotBlank(message = "Username cannot be empty")
+    @Size(min = 3, max = 30, message = "Username must be between 3 and 30 characters")
+    private String username;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+
+    public UserRequestDTO() {}
+
+    public UserRequestDTO(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
